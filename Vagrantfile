6a8975064e8803e20f898438b0e6cc72771694d1
marcis.veveris <marcis.veveris@accenture.com>
2016-09-30 11:28:39 +0300

    Add jenkins installation

--- Vagrantfile
+++ Vagrantfile
@@ -8,7 +8,7 @@ Vagrant.configure("2") do |config|
   config.vm.box = "bento/debian-8.5"
   config.vm.box_check_update = false
 
-  config.vm.network "forwarded_port", guest: 8080, host: 8080
+  config.vm.network "forwarded_port", guest: 8080, host: 8088
   config.vm.network "forwarded_port", guest: 9000, host: 9000
 
   config.vm.provider "virtualbox" do |vb|
@@ -17,4 +17,11 @@ Vagrant.configure("2") do |config|
   end
 
   config.vm.provision "shell", path: "provision.sh"
+
+  # config.vm.provision "docker" do |d|
+  #   d.run "jenkins",
+  #     image: "jenkins:alpine",
+  #     args: "-p 8080:8080 --rm -v /home/vagrant/jenkins/:/var/jenkins_home/"
+  # end
+
 end
--- provision.sh
+++ provision.sh
@@ -1,12 +1,9 @@
-echo "Setting perl:locale..."
-	export LANGUAGE=en_US.UTF-8
-	export LANG=en_US.UTF-8
-	export LC_ALL=en_US.UTF-8
-	locale-gen en_US.UTF-8
-	dpkg-reconfigure locales
+#!/bin/bash -eux
 
-echo "Updating..."
-	apt-get update > /dev/null
+export DEBIAN_FRONTEND=noninteractive
+
+# echo "Updating..."
+# 	apt-get update > /dev/null
 
 echo "Installing Vim..."
 	apt-get install vim -y > /dev/null
@@ -26,6 +23,9 @@ echo "Installing Git..."
 echo "Installing ShellCheck..."
 	apt-get install shellcheck -y > /dev/null
 
+echo "Installing dos2unix..."
+	apt-get install dos2unix -y > /dev/null
+
 echo "Installing pip2 & 3..."
 	apt-get install python-pip -y > /dev/null	
 	apt-get install python3-pip -y > /dev/null
@@ -33,16 +33,19 @@ echo "Installing pip2 & 3..."
 echo "Installing ipython2 & 3..."
 	apt-get install ipython ipython3 -y > /dev/null
 
-echo "Installing Docker..."
-	apt-get install apt-transport-https ca-certificates -y > /dev/null && \
-	apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D && \
-	echo "deb https://apt.dockerproject.org/repo debian-jessie main" > /etc/apt/sources.list.d/docker.list \
-	apt-get update > /dev/null && \
-	apt-get install docker-engine -y > /dev/null && \
-	service docker start
+# echo "Installing Docker..."
+# 	apt-get install apt-transport-https ca-certificates -y > /dev/null && \
+# 	apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D && \
+# 	echo "deb https://apt.dockerproject.org/repo debian-jessie main" > /etc/apt/sources.list.d/docker.list && \
+# 	apt-get update > /dev/null && \
+# 	apt-get install docker-engine -y > /dev/null && \
+# 	service docker start
 
 echo "Configuring docker..."
 	 groupadd docker && usermod -aG docker vagrant
+
+echo "Creating jenkins directory..."
+	mkdir /home/vagrant/jenkins/
 
 echo "Setting aliases..."
 	echo "alias c=clear" >> /home/vagrant/.bash_aliases
@@ -59,5 +62,16 @@ echo "Getting Vundle for Vi..."
 echo "Configuring swap space..."
 	fallocate -l 2G /swapfile && chmod 600 /swapfile && mkswap /swapfile && swapon /swapfile && \
 	echo "/swapfile   none    swap    sw    0   0" >> /etc/fstab
+
+echo "Installing Jenkins..."
+	wget -q -O - https://pkg.jenkins.io/debian/jenkins-ci.org.key | sudo apt-key add -
+	sh -c 'echo deb http://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
+	apt-get update > /dev/null
+	set +e
+	apt-get install jenkins -y > /dev/null
+	set -e
+	service jenkins start
+
+	service jenkins status
 
 echo "Done!"
